707076bc520416f0a605592e228383970a53e16d
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2023-02-22 12:22:04 -0500

    Included Example GraphQl Apollo Client Usage Implementation

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import {useQuery, gql} from '@apollo/client'
 
 export function Home() {
+
+// Stupid Caps Notation 
+const QUERY_LIST_OF_COUNTRIES = gql`
+{
+countries{
+  name
+  capital
+  emoji
+}
+}`;
+  // Information about the query
+  const {data, loading, error} = useQuery(QUERY_LIST_OF_COUNTRIES)
   return (
-    <div>Home</div>
-  )
+    <div className="home">Home
+    <h1> List of Countries</h1>
+    <div className="listOfCountries">
+      {loading && <h3>Data is loading...</h3>}
+      {error && <h3>{error.message}</h3>}
+      {data && data.countries && data.countries.map((country: any, key: any) => {
+        return (
+        <div key={key}>
+          <h2>
+            {country.name}
+          </h2>
+          <h4>
+            {country.capital} {country.emoji}
+          </h4>
+          </div>
+          );
+})};
+    </div>
+    </div>
+  );
 }
 
 export default Home
