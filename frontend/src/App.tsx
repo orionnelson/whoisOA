15a6abfd428f1689924ec977b8c35f9cfb35e2b4
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2023-02-22 11:29:05 -0500

    Added Yarn GraphQl and Apollo Provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes as Switch} from 'react-router-dom';
+import {ApolloClient, InMemoryCache,ApolloProvider} from '@apollo/client';
 import {Home} from './pages/Home';
 import {Search} from './pages/Search';
 import {Result} from './pages/Result';
 
+
 function App() {
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: 'https://countries.trevorblades.com/',
+  });
+
   return (
+    <ApolloProvider client={client}>
     <div className="App">
       <Router>
         <Switch>
@@ -16,6 +25,7 @@ function App() {
         </Switch>
       </Router>
     </div>
+    </ApolloProvider>
   );
 }
 
