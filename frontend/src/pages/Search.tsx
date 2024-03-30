d36c9bab306e5bd344cc5730848429441f382bd9
Priyanshu Patel <psp26072001@gmail.com>
2024-03-30 18:18:30 -0400

    init

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,91 +1,147 @@
 import React, { useState } from "react";
 import { useLazyQuery, gql } from "@apollo/client";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const QUERY_SEARCH_COUNTRY = gql`
-query Query($url: String!) {
-  website(url: $url) {
-    url
-    whois {
-      creationDate
-      domainName
-      domainStatus
-      expirationDate
-      location {
-        flagurl
-        ip
-        network
-        org
-        position {
-          latitude
-          longitude
+  query Query($url: String!) {
+    website(url: $url) {
+      url
+      whois {
+        creationDate
+        domainName
+        domainStatus
+        expirationDate
+        location {
+          flagurl
+          ip
+          network
+          org
+          position {
+            latitude
+            longitude
+          }
         }
+        nameServers {
+          name
+        }
+        registrant {
+          address
+          city
+          country
+          email
+          name
+          organization
+          state
+          zip
+        }
+        registrar {
+          abuseContactEmail
+          abuseContactPhone
+          name
+          url
+          whoisServer
+        }
+        updatedDate
       }
-      nameServers {
-        name
-      }
-      registrant {
-        address
-        city
-        country
-        email
-        name
-        organization
-        state
-        zip
-      }
-      registrar {
-        abuseContactEmail
-        abuseContactPhone
-        name
-        url
-        whoisServer
-      }
-      updatedDate
     }
   }
-}
 `;
 
 export function Search() {
   const [urlSearch, setUrlSearch] = useState("");
-  const [searchCountry, { data, loading, error }] = useLazyQuery(
-    QUERY_SEARCH_COUNTRY
-  );
-  const countryDisplay = data && data.website ? (
-    <div className="countryDisplay">
-      <h1>Url: {data.website.url || ""}</h1>
-  <h1>Creation Date: {data.website.whois.creationDate || ""}</h1>
-  <h1>Domain Name: {data.website.whois.domainName|| ""}</h1>
-  <h1>Domain Status: {data.website.whois.domainStatus|| ""}</h1>
-  <h1>Expiration Date: {data.website.whois.expirationDate|| ""}</h1>
-  <h1>Flag: {data.website.whois.location.flagurl}</h1>
-  <h1>IP: {data.website.whois.location.ip|| ""}</h1>
-  <h1>Network: {data.website.whois.location.network|| ""}</h1>
-  <h1>Organization: {data.website.whois.location.org}</h1>
-  <h1>Latitude: {data.website.whois.location.position.latitude}</h1>
-  <h1>Longitude: {data.website.whois.location.position.longitude}</h1>
-  <h1>Name Server 1: {data.website.whois.nameServers[0].name}</h1>
-  <h1>Name Server 2: {data.website.whois.nameServers[1].name}</h1>
-  <h1>Registrant Name: {data.website.whois.registrant.name}</h1>
-  <h1>Registrant Email: {data.website.whois.registrant.email}</h1>
-  <h1>Registrant Organization: {data.website.whois.registrant.organization}</h1>
-  <h1>Registrant Address: {data.website.whois.registrant.address}</h1>
-  <h1>Registrant City: {data.website.whois.registrant.city}</h1>
-  <h1>Registrant State: {data.website.whois.registrant.state}</h1>
-  <h1>Registrant Zip: {data.website.whois.registrant.zip}</h1>
-  <h1>Registrant Country: {data.website.whois.registrant.country}</h1>
-  <h1>Registrar Name: {data.website.whois.registrar.name}</h1>
-  <h1>Registrar URL: {data.website.whois.registrar.url}</h1>
-  <h1>Registrar Abuse Contact Email: {data.website.whois.registrar.abuseContactEmail}</h1>
-  <h1>Registrar Abuse Contact Phone: {data.website.whois.registrar.abuseContactPhone}</h1>
-  <h1>Registrar Whois Server: {data.website.whois.registrar.whoisServer}</h1>
-  <h1>Updated Date: {data.website.whois.updatedDate}</h1>
-    </div>
-  ) : (
-    <div className="countryDisplay">
-      <h3>https://www.example.com or https://www.example.org  </h3>
-    </div>
-  );
+  const [searchCountry, { data, loading, error }] =
+    useLazyQuery(QUERY_SEARCH_COUNTRY);
+
+  interface WhoIS {
+    creationDate?: string;
+    domainName?: string;
+    domainStatus?: string;
+    expirationDate?: string;
+    location?: {
+      flagurl?: string;
+      ip?: string;
+      network?: string;
+      org?: string;
+      position?: {
+        latitude?: string;
+        longitude?: string;
+      };
+    };
+    nameServers?: {
+      name?: string;
+    };
+    registrant?: {
+      address?: string;
+      city?: string;
+      country?: string;
+      email?: string;
+      name?: string;
+      organization?: string;
+      state?: string;
+      zip?: string;
+    };
+    registrar?: {
+      abuseContactEmail?: string;
+      abuseContactPhone?: string;
+      name?: string;
+      url?: string;
+      whoisServer?: string;
+    };
+    updatedDate?: string;
+  }
+
+  const typedData = data as {
+    website: {
+      url?: string;
+      whois?: WhoIS;
+    };
+  };
+
+  const countryDisplay =
+    data && data.website ? (
+      <div className="countryDisplay">
+        <h1>Url: {data.website.url || ""}</h1>
+        <h1>Creation Date: {data.website.whois.creationDate || ""}</h1>
+        <h1>Domain Name: {data.website.whois.domainName || ""}</h1>
+        <h1>Domain Status: {data.website.whois.domainStatus || ""}</h1>
+        <h1>Expiration Date: {data.website.whois.expirationDate || ""}</h1>
+        <h1>Flag: {data.website.whois.location.flagurl}</h1>
+        <h1>IP: {data.website.whois.location.ip || ""}</h1>
+        <h1>Network: {data.website.whois.location.network || ""}</h1>
+        <h1>Organization: {data.website.whois.location.org}</h1>
+        <h1>Latitude: {data.website.whois.location.position.latitude}</h1>
+        <h1>Longitude: {data.website.whois.location.position.longitude}</h1>
+        <h1>Name Server 1: {data.website.whois.nameServers[0].name}</h1>
+        <h1>Name Server 2: {data.website.whois.nameServers[1].name}</h1>
+        <h1>Registrant Name: {data.website.whois.registrant.name}</h1>
+        <h1>Registrant Email: {data.website.whois.registrant.email}</h1>
+        <h1>
+          Registrant Organization: {data.website.whois.registrant.organization}
+        </h1>
+        <h1>Registrant Address: {data.website.whois.registrant.address}</h1>
+        <h1>Registrant City: {data.website.whois.registrant.city}</h1>
+        <h1>Registrant State: {data.website.whois.registrant.state}</h1>
+        <h1>Registrant Zip: {data.website.whois.registrant.zip}</h1>
+        <h1>Registrant Country: {data.website.whois.registrant.country}</h1>
+        <h1>Registrar Name: {data.website.whois.registrar.name}</h1>
+        <h1>Registrar URL: {data.website.whois.registrar.url}</h1>
+        <h1>
+          Registrar Abuse Contact Email:{" "}
+          {data.website.whois.registrar.abuseContactEmail}
+        </h1>
+        <h1>
+          Registrar Abuse Contact Phone:{" "}
+          {data.website.whois.registrar.abuseContactPhone}
+        </h1>
+        <h1>
+          Registrar Whois Server: {data.website.whois.registrar.whoisServer}
+        </h1>
+        <h1>Updated Date: {data.website.whois.updatedDate}</h1>
+      </div>
+    ) : (
+      <div className="countryDisplay">
+        <h3>https://www.example.com or https://www.example.org </h3>
+      </div>
+    );
 
   return (
     <div className="search">
@@ -105,8 +161,11 @@ export function Search() {
             });
           }}
         >
-          {<a className = "search-btn"><i className="fas fa-search" aria-hidden="true"></i></a>}
-          
+          {
+            <a className="search-btn">
+              <i className="fas fa-search" aria-hidden="true"></i>
+            </a>
+          }
         </button>
       </div>
 
